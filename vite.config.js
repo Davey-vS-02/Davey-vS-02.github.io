46620a46b800a708cab324b499869a43587bb462
David-vS-02 <davidvanstaden02@gmail.com>
2025-08-21 21:03:04 +0200

    Fresh

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/public/build/',
-  plugins: [
-    laravel({
-      input: 'resources/js/app.js',
-      refresh: true,
-    }),
-    vue({
-      template: {
-        transformAssetUrls: {
-          base: null,
-          includeAbsolute: false,
-        },
-      },
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    base: '/'
 });
